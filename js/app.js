3f13f95307b629f3eb154349a7ea6efe3828fb64
ahmadhawamdeh <ahmadhawamdeh313@gmail.com>
2019-10-21 21:37:25 +0300

    edit lab12

--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
 'use strict';
 function Product(title, src) {
-  this.title = title;
-  this.src = src;
-  this.clickCtr = 0;
-  this.shownCtr = 0;
-  Product.all.push(this);
+    this.title = title;
+    this.src = src;
+    this.clickCtr = 0;
+    this.shownCtr = 0;
+    Product.all.push(this);
 }
+/////////////// step 1
+
 Product.roundCtr = 0;
 Product.roundLimit = 25;
 Product.all = [];
 
+/////////////// step 2
+
 Product.container = document.getElementById('productcont');
-
-///////////////////// step 1
-
 Product.leftImage = document.getElementById('left-img');
 Product.middImage = document.getElementById('midd-img');
-Product.rightImage = document.getElementById('right-image');
-
+Product.rightImage = document.getElementById('right-img');
 Product.leftTitle = document.getElementById('left-title');
 Product.middTitle = document.getElementById('midd-title');
 Product.rightTitle = document.getElementById('right-title');
 
-///////////////////// step 2
+/////////////// step 3
 
 Product.leftObject = null;
-Product.middObject= null;
+Product.middObject = null;
 Product.rightObject = null;
 
-///////////////////// step 3
+/////////////// step 4
 
-new Product('Bag', 'images/bag.jpg');
-new Product('Banana', 'images/banana.jpg');
-new Product('Bathroom', 'images/bathroom.jpg');
-new Product('Boots', 'images/boots.jpg');
+new Product('bag', 'images/bag.jpg');
+new Product('banana', 'images/banana.jpg');
+new Product('bathroom', 'images/bathroom.jpg');
+new Product('boots', 'images/boots.jpg');
 new Product('breakfast', 'images/breakfast.jpg');
 new Product('bubblegum', 'images/bubblegum.jpg');
 new Product('chair', 'images/chair.jpg');
@@ -51,137 +51,150 @@ new Product('usb', 'images/usb.gif');
 new Product('water-can', 'images/water-can.jpg');
 new Product('wine-glass', 'images/wine-glass.jpg');
 
-///////////////////// step 4
+/////////////// step 5
 
-function renderNewProduct() {
+function renderBus() {
+    var forbidden = [Product.leftObject, Product.middObject, Product.rightObject];
+    do {
+        Product.leftObject = getRandomitems();
+    } while (forbidden.includes(Product.leftObject))
+    forbidden.push(Product.leftObject);
+    do {
+        Product.middObject = getRandomitems();
+    } while (forbidden.includes(Product.middObject))
+    forbidden.push(Product.middObject);
+    do {
+        Product.rightObject = getRandomitems();
+    } while (forbidden.includes(Product.rightObject));
 
-  var forbidden = [Product.leftObject, Product.middObject, Product.rightObject];
+    /////////////// step 6
 
-///////////////////// step 5
+    Product.leftObject.shownCtr++;
+    Product.middObject.shownCtr++;
+    Product.rightObject.shownCtr++;
 
-  do {
-    Product.leftObject = getRandomProduct();
-  } while (forbidden.includes(Product.leftObject))
-  forbidden.push(Product.leftObject);
+    /////////////// step 7
 
-  do {
-    Product.middObject = getRandomProduct();
-  } while (forbidden.includes(Product.middObject))
-  forbidden.push(Product.middObject);
+    var leftImageElement = Product.leftImage;
+    var middimageElement = Product.middImage;
+    var rightImageElement = Product.rightImage;
 
-  do {
-    Product.rightObject = getRandomProduct();
-  } while (forbidden.includes(Product.rightObject));
+    /////////////// step 8
 
-///////////////////// step 6
+    leftImageElement.setAttribute('src', Product.leftObject.src);
+    leftImageElement.setAttribute('alt', Product.leftObject.title);
 
-  Product.leftObject.shownCtr++;
-  Product.middObject.shownCtr++;
-  Product.rightObject.shownCtr++;
+    middimageElement.setAttribute('src', Product.middObject.src);
+    middimageElement.setAttribute('alt', Product.middObject.title);
 
-///////////////////// step 7
+    rightImageElement.setAttribute('src', Product.rightObject.src);
+    rightImageElement.setAttribute('alt', Product.rightObject.title);
 
-  var leftProductImageElement = Product.leftImage;
-  var middProductImageElement = Product.middImage;
-  var rightProductImageElement = Product.rightImage;
+    /////////////// step 9
 
-///////////////////// step 8
+    Product.leftTitle.textContent = Product.leftObject.title;
+    Product.middTitle.textContent = Product.middObject.title;
+    Product.rightTitle.textContent = Product.rightObject.title;
 
-  leftProductImageElement.setAttribute('src', Product.leftObject.src);
-  leftProductImageElement.setAttribute('alt', Product.leftObject.title);
-
-  middProductImageElement.setAttribute('src', Product.middObject.src);
-  middProductImageElement.setAttribute('alt', Product.middObject.title);
-
-  rightProductImageElement.setAttribute('src', Product.rightObject.src);
-  rightProductImageElement.setAttribute('alt', Product.rightObject.title);
-
-///////////////////// step 9
-
-  Product.leftTitle.textContent = Product.leftObject.title;
-  Product.middTitle.textContent = Product.middObject.title;
-  Product.rightTitle.textContent = Product.rightObject.title;
-
-///////////////////// step 10
+    /////////////// step 10
 }
-function getRandomProduct() {
-  var index = Math.floor(Math.random() * Product.all.length);
-  return Product.all[index];
+function getRandomitems() {
+    var index = Math.floor(Math.random() * Product.all.length);
+    return Product.all[index];
 }
 function randomInRange(min, max) {
-  var range = max - min + 1;
-  var rand = Math.floor(Math.random() * range) + min
-  return rand;
+    var range = max - min + 1;
+    var rand = Math.floor(Math.random() * range) + min
+    return rand;
 }
-function updateTotals() {
-  var tableBody = document.getElementById('newpro');
-
-  tableBody.innerHTML = '';
-  for (var i = 0; i < Product.all.length; i++) {
-    var produc = Product.all[i];
-    var row = addElement('tr', tableBody);
-    addElement('td', row, produc.title);
-    addElement('td', row, '' + produc.clickCtr);
-    addElement('td', row, '' + produc.shownCtr);
-  }
-}
-function addElement(tag, container, text) {
-  var element = document.createElement(tag);
-  container.appendChild(element);
-  if (text) {
-    element.textContent = text;
-  }
-  return element;
-}
-function clickHandler(event) {
-  var clickedId = event.target.id;
-  var ProductClicked;
-  if (clickedId === 'left-img') {
-    ProductClicked = Product.leftObject;
-  } else if (clickedId === 'midd-img') {
-    ProductClicked = Product.middObject;
-  } else if (clickedId === 'right-image') {
-    ProductClicked = Product.rightObject;
-  } else {
-    alert('Please just click on one of the pictures products listed on the screen, not outside the picture frame ^_^')
-  }
-  if (ProductClicked) {
-    ProductClicked.clickCtr++;
-    Product.roundCtr++;
-    updateTotals();
-    if (Product.roundCtr === Product.roundLimit) {
-      alert('No more clicking');
-      Product.container.removeEventListener('click', clickHandler);
-    } else {
-      renderNewProduct();
+function Totalsproducts() {
+    var tableBody = document.getElementById('newpro');
+    tableBody.innerHTML = '';
+    for (var i = 0; i < Product.all.length; i++) {
+        var item = Product.all[i];
+        var row = addElement('tr', tableBody);
+        addElement('td', row, item.title);
+        addElement('td', row, '' + item.clickCtr);
+        addElement('td', row, '' + item.shownCtr);
+        addElement('td', row, '' + ' had ' + item.clickCtr + ' votes ' + ' and was shown ' + item.shownCtr);
     }
-  }
 }
+/////////////// step 11
 
-///////////////////// step 11
+function addElement(tag, container, text) {
+    var element = document.createElement(tag);
+    container.appendChild(element);
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
+}
+function clickItem(event) {
+    var clickedId = event.target.id;
+    var itemClicked;
+    if (clickedId === 'left-img') {
+        itemClicked = Product.leftObject;
+    } else if (clickedId === 'midd-img') {
+        itemClicked = Product.middObject;
+    } else if (clickedId === 'right-img') {
+        itemClicked = Product.rightObject;
+    } else {
+        console.log('Um, what was clicked on???', clickedId);
+    }
+    if (itemClicked) {
+        itemClicked.clickCtr++;
+        Product.roundCtr++;
+        Totalsproducts();
+        if (Product.roundCtr === Product.roundLimit) {
+            rendermallitems();
+            alert('No more clicking !');
+            Product.container.removeEventListener('click', clickItem);
+        } else {
+            renderBus();
+        }
+    }
+}
+/////////////// step 12
 
-Product.container.addEventListener('click', clickHandler);
-updateTotals();
-renderNewProduct();
+function rendermallitems() {
+    var MallArray = [];
+    var ClickedArray = [];
+    var shownArray = [];
+    for (let i = 0; i < Product.all.length; i++) {
+        var MallInstenc = Product.all[i];
+        MallArray.push(MallInstenc.title + ' click');
+        MallArray.push(MallInstenc.title + ' Shown');
+        ClickedArray.push(MallInstenc.clickCtr);
+        shownArray.push(MallInstenc.shownCtr);
+    }
+    /////////////// step 13
 
-///////////////////// step 12
-var ctx = document.getElementById('myChart').getContext('2d');
+    var ctx = document.getElementById('Chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck ',
+                'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'
+            ],
+            datasets: [{
+                label: 'Item click',
+                backgroundColor: 'white',
+                borderColor: 'black',
+                data: ClickedArray,
+            },
+            {
+                label: 'Item Shown',
+                backgroundColor: 'red',
+                borderColor: 'black',
+                data: shownArray,
+            }
+            ],
+            options: {}
+        }
+    });
+}
+/////////////// step 14
 
-console.log('Chart : ', Chart);
-var chart = new Chart(ctx, {
-  type: 'line',
-
-  data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'My First Data',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45]
-      }
-    ]
-
-  },
-  options: {}
-});
+Product.container.addEventListener('click', clickItem);
+Totalsproducts();
+renderBus();
